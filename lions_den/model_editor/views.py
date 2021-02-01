4fe39b3012f19f5ae5601e189b082772a0c0063e
Pedro Ferreira <pmb.ferreira93@gmail.com>
2021-02-01 20:20:02 +0000

    More minor refactoring

--- lions_den/model_editor/views.py
+++ lions_den/model_editor/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # noinspection PyUnresolvedReferences
