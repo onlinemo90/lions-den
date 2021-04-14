677f2c264bf05018f7fe9906ab92fc904f23d64a
Pedro Ferreira <pmb.ferreira93@gmail.com>
2021-04-14 01:12:09 +0100

    The Subject attribute formset is now more dynamic: Clicking the button to add a new attribute presents the user with the available categories. If no categories are available, the button will not display. Submitting to add a new category will add an empty form to the formset, respecting the order given by the category position. The category is then removed from the list so it cannot be added more than once. Once no categories are available, the 'add attribute' button will be hidden. Validation of the forms still occurs and works seamlessly with the subject fields, meaning if either the subject form and the attributes formset each prevent the other from saving but never erase data from each other.

--- lions_den/zoo_editor/forms.py
+++ lions_den/zoo_editor/forms.py
@@ -116,10 +116,23 @@ class GroupForm(BaseSubjectForm):
 		self.fields['individuals'].widget.attrs.update({'hidden': True})
 
 
-class AttributeCategoryForm(BaseModelForm):
+class AvailableSubjectAttributeCategoriesForm(BaseModelForm):
 	class Meta:
 		model = AttributeCategory
 		fields = ('name',)
+	
+	def __init__(self, subject, exclude_categories, *args, **kwargs):
+		super().__init__(zoo_id=subject.zoo.id, *args, **kwargs)
+		
+		used_categories = [attribute.category.id for attribute in subject.attributes.all()] + exclude_categories
+		self.fields['name'] = forms.ModelChoiceField(
+			label='Category',
+			empty_label='',
+			queryset=AttributeCategory.objects.using(subject.zoo.id).exclude(id__in=used_categories)
+		)
+	
+	def save(self, commit=True):
+		raise AttributeError('This form is not meant to alter data!')
 
 
 def get_attributes_formset(subject, *args, **kwargs):
@@ -127,11 +140,30 @@ def get_attributes_formset(subject, *args, **kwargs):
 	class SubjectAttributeForm(BaseModelForm):
 		class Meta:
 			model = subject.__class__.get_attribute_model()
-			fields = ('attribute',)
+			fields = ('category', 'attribute')
 		
 		def __init__(self, *args, **kwargs):
 			super().__init__(*args, **kwargs)
-			self.fields['attribute'].label = self.instance.category.name
+
+			self.instance.subject_id = subject.id
+
+			# Set additional hidden fields to allow dynamic adding of new forms
+			if hasattr(self.instance, 'category'): # when basing on an existing attribute instance
+				self.fields['attribute'].label = self.instance.category.name
+				initial_category_name = self.instance.category.name
+				initial_category_position = self.instance.category.position
+			elif hasattr(self, 'data') and self.data: # when reloading the form if invalid
+				initial_category_name = self.data.get(self.prefix + '-category_name')
+				self.fields['attribute'].label = initial_category_name
+				initial_category_position = self.data.get(self.prefix + '-category_position')
+			else: # for formset.empty_form, since it will not have a category bound to it
+				self.fields['attribute'].label = '__CATEGORY_NAME_PLACEHOLDER__'
+				initial_category_name = ''
+				initial_category_position = ''
+			
+			self.fields['category'] = forms.ModelChoiceField(widget=forms.HiddenInput, queryset=AttributeCategory.objects.using(self.zoo_id).all())
+			self.fields['category_name'] = forms.CharField(widget=forms.HiddenInput, initial=initial_category_name)
+			self.fields['category_position'] = forms.CharField(widget=forms.HiddenInput, initial=initial_category_position)
 	
 	
 	# Define formset for multiple attributes
@@ -140,19 +172,30 @@ def get_attributes_formset(subject, *args, **kwargs):
 		form=SubjectAttributeForm,
 		extra=0,
 		can_delete=True,
-		can_order=False
+		can_order=False,
+		max_num=AttributeCategory.objects.using(subject.zoo.id).count()
 	)
 	
 	# Define formset for attributes specific to a subject (Species or Individual)
 	class SubjectAttributesFormSet(BaseSubjectAttributesFormSet):
 		def __init__(self, subject, *args, **kwargs):
 			super().__init__(
-				queryset=subject.__class__.get_attribute_model().objects.using(subject.zoo.id).filter(subject_id=subject.id).order_by('category__position').all(),
+				queryset=subject.__class__.get_attribute_model().objects.using(subject.zoo.id).filter(subject_id=subject.id).all(),
 				form_kwargs={'zoo_id': subject.zoo.id},
 				*args, **kwargs
 			)
+		
+		def __iter__(self):
+			"""" Defines the order in which forms are rendered """
+			return sorted(self.forms, key=lambda form : str(form.fields['category_position'].initial)).__iter__()
 	
 	return SubjectAttributesFormSet(subject, *args, **kwargs)
+
+
+class AttributeCategoryForm(BaseModelForm):
+	class Meta:
+		model = AttributeCategory
+		fields = ('name',)
 
 
 def get_attribute_categories_formset(zoo_id, *args, **kwargs):
@@ -186,26 +229,3 @@ def get_attribute_categories_formset(zoo_id, *args, **kwargs):
 			set_categories_position(1)
 	
 	return AttributeCategoriesFormset(zoo_id, *args, **kwargs)
-
-
-def get_new_attribute_form(subject, *args, **kwargs):
-	used_categories = [attribute.category.id for attribute in subject.attributes.all()]
-	category_queryset = AttributeCategory.objects.using(subject.zoo.id).exclude(id__in=used_categories)
-	
-	if len(category_queryset) > 0:
-		class NewSubjectAttributeForm(BaseModelForm):
-			class Meta:
-				model = subject.__class__.get_attribute_model()
-				fields = ('category', 'attribute')
-				labels = {'category': 'Header', 'attribute': 'Text'}
-			
-			def __init__(self, *args, **kwargs):
-				super().__init__(zoo_id=subject.zoo.id, *args, **kwargs)
-				self.fields['category'] = forms.ModelChoiceField(queryset=category_queryset, empty_label='')
-			
-			def save(self, commit=True):
-				self.instance.subject = subject
-				super().save(commit)
-		
-		return NewSubjectAttributeForm(*args, **kwargs)
-	return None
