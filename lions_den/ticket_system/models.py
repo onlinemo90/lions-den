b0148eb51da24f75ae2b9b562a22b26c5665add5
Pedro Ferreira <pmb.ferreira93@gmail.com>
2021-04-16 23:12:57 +0100

    Initial forms, view and DB changes. Added first_name and last_name fields to ZooUser model

--- lions_den/ticket_system/models.py
+++ lions_den/ticket_system/models.py
@@ -8,11 +8,13 @@ class Ticket(models.Model):
 	id = models.AutoField(primary_key=True)
 	title = models.CharField(max_length=48)
 	description = models.TextField()
-	reporter = models.ForeignKey(to=ZooUser, related_name='reporter', on_delete=models.PROTECT, null=False)
-	assignee = models.ForeignKey(to=ZooUser, related_name='assignee', on_delete=models.PROTECT, null=True, default=None)
+	reporter = models.ForeignKey(ZooUser, related_name='reported_tickets', on_delete=models.PROTECT, blank=True, default=None)
+	assignee = models.ForeignKey(ZooUser, related_name='assigned_tickets', on_delete=models.PROTECT, blank=True, default=None)
 	created_date = models.DateTimeField(auto_now_add=True)
 	last_updated_date = models.DateTimeField(auto_now=True)
+	closed_date = models.DateTimeField(blank=True)
 	# TODO: add watchers
+	# TODO: add fix versions
 	
 	def __str__(self):
 		return str(self.id) + ' - ' + self.title
@@ -20,11 +22,14 @@ class Ticket(models.Model):
 
 class Comment(models.Model):
 	id = models.AutoField(primary_key=True)
-	ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
-	creator = models.ForeignKey(to=ZooUser, related_name='creator', on_delete=models.PROTECT)
+	ticket = models.ForeignKey(Ticket, related_name='comments', on_delete=models.CASCADE)
+	creator = models.ForeignKey(ZooUser, related_name='comments', on_delete=models.PROTECT)
 	text = models.TextField()
 	added_date = models.DateTimeField(auto_now_add=True)
 	last_updated_date = models.DateTimeField(auto_now=True)
 	
 	def __str__(self):
 		return self.text
+
+
+# TODO: Add release versions
