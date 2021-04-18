dd828729963abd59b3d3e05035c520ca9ddd44bd
Pedro Ferreira <pmb.ferreira93@gmail.com>
2021-04-18 01:15:27 +0100

    Added ability to create, update and assign tickets, and to add comments. Created the ticket page.

--- lions_den/ticket_system/forms.py
+++ lions_den/ticket_system/forms.py
@@ -1,16 +1,39 @@
 from django import forms
 
 from .models import Ticket, Comment
+from zoo_auth.models import ZooUser
 
 
 class TicketForm(forms.ModelForm):
 	class Meta:
 		model = Ticket
-		fields = ('title', 'description')
+		fields = ('title', 'type', 'priority', 'description')
 	
 	def save(self, reporter, commit=True):
 		self.instance.reporter = reporter
 		super().save(commit)
+
+
+class AssignTicketForm(forms.ModelForm):
+	class Meta:
+		model = Ticket
+		fields = ('assignee',)
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['assignee'] = forms.ModelChoiceField(queryset=ZooUser.objects.filter(is_staff=True))
+	
+	def save(self, commit=True):
+		super().save(commit)
+		if self.instance.action == Ticket.Actions.WAIT:
+			self.instance.action = Ticket.Actions.IN_ANALYSIS
+			self.instance.save()
+
+
+class UpdateTicketForm(forms.ModelForm):
+	class Meta:
+		model = Ticket
+		fields = ('status', 'action')
 
 
 class CommentForm(forms.ModelForm):
