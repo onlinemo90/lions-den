860eaee83a4fb6987a900ee1bd4e1b93a4c3282b
Pedro Ferreira <pmb.ferreira93@gmail.com>
2021-05-08 01:06:08 +0100

    Moved signal notification logic for creation of new items from pre_save to post_save

--- lions_den/ticket_system/signals.py
+++ lions_den/ticket_system/signals.py
@@ -1,11 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.template.loader import render_to_string
 
-# noinspection PyUnresolvedReferences
-from zoo_auth.models import ZooUser
-
-from .models import Ticket, Comment
+from .models import Ticket, Comment, TicketAttachment
 
 
 def get_field_display(instance, field):
@@ -23,37 +21,37 @@ def get_instance_changes(new_instance, old_instance=None):
 				changes[field.verbose_name]['old_value'] = get_field_display(old_instance, field)
 	return changes
 
-@receiver(pre_save, sender=Ticket)
-def notify_ticket_changes(sender, instance, **kwargs):
-	try:
-		old_ticket = sender.objects.get(pk=instance.pk)
-	except sender.DoesNotExist:
-		ZooUser.notify_users(
-			users=ZooUser.objects.filter(id=instance.reporter.id),
-			subject=f'{instance.reporter} created a ticket: {instance.title}',
+@receiver(post_save, sender=Ticket)
+def notify_ticket_created(sender, instance, created, **kwargs):
+	if created:
+		get_user_model().notify_users(
+			users=get_user_model().objects.filter(id=instance.reporter.id),
+			subject=f'{instance.reporter} created a ticket: {instance}',
 			html_message=render_to_string(
 				'ticket_system/emails/notification.html',
 				{'context': get_instance_changes(instance)}
 			)
 		)
-	else:
+
+@receiver(pre_save, sender=Ticket)
+def notify_ticket_changed(sender, instance, **kwargs):
+	if instance.id is not None:
+		old_ticket = sender.objects.get(pk=instance.pk)
 		changes = get_instance_changes(instance, old_ticket)
 		if changes:
-			ZooUser.notify_users(
+			get_user_model().notify_users(
 				users=instance.watchers.all(),
-				subject=f'{instance.last_updater} edited ticket: {instance.title}',
+				subject=f'{instance.last_updater} edited ticket: {instance}',
 				html_message=render_to_string(
 					'ticket_system/emails/notification.html',
 					{'context': changes, 'edit_instance': True}
 				)
 			)
 
-@receiver(pre_save, sender=Comment)
-def notify_comment_changes(sender, instance, **kwargs):
-	try:
-		old_comment = sender.objects.get(pk=instance.pk)
-	except sender.DoesNotExist:
-		ZooUser.notify_users(
+@receiver(post_save, sender=Comment)
+def notify_comment_created(sender, instance, created, **kwargs):
+	if created:
+		get_user_model().notify_users(
 			users=instance.ticket.watchers.all(),
 			subject=f'{instance.creator} added a comment on ticket: {instance.ticket}',
 			html_message=render_to_string(
@@ -61,10 +59,14 @@ def notify_comment_changes(sender, instance, **kwargs):
 				{'context': get_instance_changes(instance)}
 			)
 		)
-	else:
+
+@receiver(pre_save, sender=Comment)
+def notify_comment_changed(sender, instance, **kwargs):
+	if instance.id is not None:
+		old_comment = sender.objects.get(pk=instance.pk)
 		changes = get_instance_changes(instance, old_comment)
 		if changes:
-			ZooUser.notify_users(
+			get_user_model().notify_users(
 				users=instance.ticket.watchers.all(),
 				subject=f'{instance.creator} edited a comment in ticket: {instance.ticket}',
 				html_message=render_to_string(
