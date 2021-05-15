8bbe2736e45a3cb027bec4622087cd8ca3403118
Pedro Ferreira <pmb.ferreira93@gmail.com>
2021-05-15 15:52:00 +0100

    Removing last remnants of the use of signals for email notifications

--- lions_den/ticket_system/apps.py
+++ lions_den/ticket_system/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class TicketSystemConfig(AppConfig):
     name = 'ticket_system'
     verbose_name = 'Ticket System'
-    
-    def ready(self):
-        # noinspection PyUnresolvedReferences
-        import ticket_system.signals
