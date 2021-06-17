fd2e79003b0c16bcd18db5df1734d1101e961ca9
onlinemo90 <moritz.fritzsche@gmx.ch>
2021-06-17 08:33:06 +0100

    password reset functionality - added templates to support to request reset email and actually reset the PW. - PW reset accessible via hamburger menu in login screen.

--- lions_den/zoo_auth/urls.py
+++ lions_den/zoo_auth/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
 	path('user/change-password/', auth_views.PasswordChangeView.as_view(template_name='zoo_auth/change_password.html', success_url=reverse_lazy('zoo_index')), name='change_password'),
 	path('user/preferences/', views.PreferencesView.as_view(), name='user_preferences'),
 	path('user/notifications/', views.NotificationsView.as_view(), name='user_notifications'),
+	path('password_reset/', auth_views.PasswordResetView.as_view(template_name='zoo_auth/password_reset.html'), name='password_reset'),
+	path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='zoo_auth/password_reset_done.html'), name='password_reset_done'),
+	path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="zoo_auth/password_reset_confirm.html"), name='password_reset_confirm'),
+	path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='zoo_auth/password_reset_complete.html'), name='password_reset_complete'),
 ]
